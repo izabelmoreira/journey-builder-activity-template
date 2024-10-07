75d08bf69cd7f9bb420855c6abfdccaf1d629653
Izabel Moreira <izabelassuncaom@gmail.com>
2024-10-07 10:39:35 -0300

    mexendo na função execute

--- routes/activity.js
+++ routes/activity.js
@@ -68,58 +68,31 @@ exports.save = function (req, res) {
   res.send(200, 'Save');
 };
 
-// /*
-//  * POST Handler for /execute/ route of Activity.
-//  */
-// exports.execute = function (req, res) {
-//   // example on how to decode JWT
-//   JWT(req.body, process.env.jwtSecret, (err, res) => {
-//     // verification error -> unauthorized request
-//     if (err) {
-//       console.error(err);
-//       return res.status(401).end();
-//     }
-
-//     if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-//       // decoded in arguments
-//       var decodedArgs = decoded.inArguments[0];
-
-//       // logData(req);
-//       //eu fiz a correção aqui
-//       console.log('Requisição: ', decodedArgs);
-//       res.send(200, 'Execute');
-//     } else {
-//       console.error('inArguments invalid.');
-//       return res.status(400).end();
-//     }
-//   });
-// };
 /*
  * POST Handler for /execute/ route of Activity.
  */
-
 exports.execute = function (req, res) {
-  console.log('5 -- For Execute');
-  var requestBody = req.body;
-  console.log('Request Body' + JSON.stringify(requestBody));
-  res.send(200, 'Publish');
-  // Used to decode JWT
-  // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-  //     // verification error -> unauthorized request
-  //     if (err) {
-  //         console.error(err);
-  //         return res.status(401).end();
-  //     }
-  //     if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-  //         // decoded in arguments
-  //         var decodedArgs = decoded.inArguments[0];
-  //         logData(req);
-  //         res.send(200, 'Execute');
-  //     } else {
-  //         console.error('inArguments invalid.');
-  //         return res.status(400).end();
-  //     }
-  // });
+  // example on how to decode JWT
+  JWT(req.body, process.env.jwtSecret, (err, res) => {
+    // verification error -> unauthorized request
+    if (err) {
+      console.error(err);
+      return res.status(401).end();
+    }
+
+    if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+      // decoded in arguments
+      var decodedArgs = decoded.inArguments[0];
+
+      // logData(req);
+
+      console.log('Requisição: ', decodedArgs);
+      res.send(200, 'Execute');
+    } else {
+      console.error('inArguments invalid.');
+      return res.status(400).end();
+    }
+  });
 };
 
 /*
